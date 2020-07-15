b0bb5adebdafe34b7ebe422b03815b6d616c400c
Thu Vo <votranminhthu00@gmail.com>
2020-07-15 17:34:18 -0400

    add black fragments and implemeted it in Main Activity

--- TheFoodieNetwork/app/src/main/java/com/fbu/thefoodienetwork/activities/MainActivity.java
+++ TheFoodieNetwork/app/src/main/java/com/fbu/thefoodienetwork/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.fbu.thefoodienetwork.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,11 +12,16 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.fbu.thefoodienetwork.R;
 import com.fbu.thefoodienetwork.ZomatoRequest;
 import com.fbu.thefoodienetwork.databinding.ActivityMainBinding;
+import com.fbu.thefoodienetwork.fragments.ComposeFragment;
+import com.fbu.thefoodienetwork.fragments.GlobeFragment;
+import com.fbu.thefoodienetwork.fragments.HomeFragment;
 import com.fbu.thefoodienetwork.models.Location;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 import java.util.List;
@@ -27,10 +34,38 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_compose:
+                        fragment = new ComposeFragment();
+                        Log.i(TAG, "menu item: " + "compose");
+                        break;
+                    case R.id.action_globe:
+                        fragment = new GlobeFragment();
+                        Log.i(TAG, "menu item: " + "globe");
+                        break;
+                    case R.id.action_home:
+                    default:
+                        fragment = new HomeFragment();
+                        Log.i(TAG, "menu item: " + "home");
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.containerFrameLayout, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        binding.bottomNavigation.setSelectedItemId(R.id.action_home);
     }
 
     @Override
