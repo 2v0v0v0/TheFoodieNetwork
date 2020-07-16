52803912f0e7768117ae5265fb06f83f7530d640
Thu Vo <votranminhthu00@gmail.com>
2020-07-16 11:28:45 -0400

    set basic layout for compose fragment

--- TheFoodieNetwork/app/src/main/java/com/fbu/thefoodienetwork/fragments/ComposeFragment.java
+++ TheFoodieNetwork/app/src/main/java/com/fbu/thefoodienetwork/fragments/ComposeFragment.java
@@ -9,42 +9,24 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.fbu.thefoodienetwork.R;
+import com.fbu.thefoodienetwork.databinding.FragmentComposeBinding;
 import com.fbu.thefoodienetwork.models.Restaurant;
 
 import org.parceler.Parcels;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ComposeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ComposeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private FragmentComposeBinding binding;
     private static final String ARG_RESTAURANT = "selectedRestaurant";
-    //private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private Restaurant mRestaurant;
-    //private String mParam2;
 
     public ComposeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment ComposeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ComposeFragment newInstance(Restaurant restaurant) {
         ComposeFragment fragment = new ComposeFragment();
         Bundle args = new Bundle();
         args.putParcelable(ARG_RESTAURANT, Parcels.wrap(restaurant));
-        //args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,15 +36,18 @@ public class ComposeFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mRestaurant = (Restaurant) Parcels.unwrap(getArguments().getParcelable(ARG_RESTAURANT));
-            //mRestaurant = getArguments().getParcelable(ARG_RESTAURANT);
-            //mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compose, container, false);
+        // binding.
+        binding = FragmentComposeBinding.inflate(getLayoutInflater(), container, false);
+        View view = binding.getRoot();
+        if (mRestaurant != null){
+            binding.restaurantInfoTextView.setText(mRestaurant.getName());
+        }
+        return view;
     }
 }
