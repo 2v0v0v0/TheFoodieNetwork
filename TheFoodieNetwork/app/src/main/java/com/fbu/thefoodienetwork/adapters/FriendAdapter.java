3779dbf902dbcd92d3c27abf2bb1fa63fd824efa
Thu Vo <votranminhthu00@gmail.com>
2020-07-20 20:22:52 -0400

    show friend status on recycler view

--- TheFoodieNetwork/app/src/main/java/com/fbu/thefoodienetwork/adapters/FriendAdapter.java
+++ TheFoodieNetwork/app/src/main/java/com/fbu/thefoodienetwork/adapters/FriendAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.fbu.thefoodienetwork.CurrentUserUtilities;
 import com.fbu.thefoodienetwork.R;
+import com.fbu.thefoodienetwork.databinding.ItemFriendBinding;
+import com.fbu.thefoodienetwork.databinding.ItemReviewBinding;
 import com.parse.ParseUser;
 
 import java.util.List;
@@ -41,8 +44,9 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
     @NonNull
     @Override
     public FriendAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View friendView = LayoutInflater.from(context).inflate(R.layout.item_friend, parent, false);
-        return new FriendAdapter.ViewHolder(friendView);
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ItemFriendBinding binding = ItemFriendBinding.inflate(layoutInflater, parent, false);
+        return new FriendAdapter.ViewHolder(binding);
     }
 
     @Override
@@ -57,7 +61,7 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
         } else if (receivedFRList.contains(userID)){
             relationStatus = RECEIVED_FR;
         }
-
+        Log.i("onBindViewHolder", aUser.getUsername() + " " + relationStatus);
         try {
             holder.bind(aUser, relationStatus);
         } catch (Exception e) {
@@ -71,29 +75,37 @@ public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView profileImageView;
-        TextView screennameTextView;
-        TextView usernameTextview;
-        ImageView addFriendImageView;
-        TextView pendingTextView;
+        private ImageView profileImageView;
+        private TextView screennameTextView;
+        private TextView usernameTextview;
+        private ImageView addFriendImageView;
+        private TextView pendingTextView;
+        private LinearLayout receivedFRButtonsContainer;
+        private final ItemFriendBinding binding;
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            profileImageView = itemView.findViewById(R.id.profileImageView);
-            screennameTextView = itemView.findViewById(R.id.screenNameTextView);
-            usernameTextview = itemView.findViewById(R.id.usernameTextView);
-            addFriendImageView = itemView.findViewById(R.id.addFriendImageView);
-            pendingTextView = itemView.findViewById(R.id.pendingTextView);
+        public ViewHolder(ItemFriendBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+            profileImageView = binding.profileImageView;
+            screennameTextView = binding.screenNameTextView;
+            usernameTextview = binding.usernameTextView;
+            addFriendImageView = binding.addFriendImageView;
+            pendingTextView = binding.pendingTextView;
+            receivedFRButtonsContainer = binding.receivedFRButtonsContainer;
         }
 
         public void bind(ParseUser aUser, int relationStatus) throws Exception {
             //set add friend button to only users that not in current user's friend list
+            Log.i("bind", aUser.getUsername() + " " + relationStatus);
             switch (relationStatus){
                 case IS_STRANGER:
                     addFriendButtonListener();
                     break;
                 case SENT_FR:
                     pendingTextView.setVisibility(View.VISIBLE);
+                    break;
+                case RECEIVED_FR:
+                    receivedFRButtonsContainer.setVisibility(View.VISIBLE);
                     break;
                 default:
                     break;
