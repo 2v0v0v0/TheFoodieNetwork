22c531f3cef37e74ae2567c86e47c5b6880bc9fe
Thu Vo <votranminhthu00@gmail.com>
2020-07-24 19:06:12 -0400

    add recyclerview to profile activity

--- TheFoodieNetwork/app/src/main/java/com/fbu/thefoodienetwork/activities/ProfileActivity.java
+++ TheFoodieNetwork/app/src/main/java/com/fbu/thefoodienetwork/activities/ProfileActivity.java
@@ -6,18 +6,33 @@ import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.fbu.thefoodienetwork.R;
+import com.fbu.thefoodienetwork.adapters.ReviewAdapter;
 import com.fbu.thefoodienetwork.databinding.ActivityProfileBinding;
 import com.fbu.thefoodienetwork.keys.UserKeys;
+import com.fbu.thefoodienetwork.models.ParseReview;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //TODO change LoginActivity goMainActivity when finish
 public class ProfileActivity extends AppCompatActivity {
     private final static String TAG = "ProfileActivity";
     private ActivityProfileBinding binding;
     private ParseUser user = ParseUser.getCurrentUser();
+    protected ReviewAdapter reviewAdapter;
+    protected List<ParseReview> allReviews;
+    private LinearLayoutManager layoutManager;
+    private RecyclerView reviewRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,14 @@ public class ProfileActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        reviewRecyclerView = binding.postsRecyclerView;
+        allReviews = new ArrayList<>();
+        reviewAdapter = new ReviewAdapter(this, allReviews);
+        reviewRecyclerView.setAdapter(reviewAdapter);
+        layoutManager = new LinearLayoutManager(this);
+        reviewRecyclerView.setLayoutManager(layoutManager);
+
+        queryReviews();
 
         binding.editButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,11 +57,8 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        ParseFile image = user.getParseFile(UserKeys.profileImage);
-        Log.i(TAG, image.getUrl());
-        Glide.with(this).load(image.getUrl()).centerCrop().circleCrop().into(binding.profileImage);
-
         try {
+
             if (user.getString(UserKeys.screenName) == null || user.getString(UserKeys.screenName).trim().equals("")) {
                 binding.textViewScreenName.setText(user.getUsername());
             } else {
@@ -47,11 +67,19 @@ public class ProfileActivity extends AppCompatActivity {
             }
 
 
-            /*if (image != null) {
+            try {
+                binding.bioTextView.setText(user.getString(UserKeys.bio));
+            } catch (Exception e) {
+                Log.i(TAG, "error:"+e.toString());
+            }
+
+            ParseFile image = user.getParseFile(UserKeys.profileImage);
+            if (image != null) {
                 Glide.with(this).load(image.getUrl()).centerCrop().circleCrop().into(binding.profileImage);
             } else {
-                //Glide.with(this).load(R.drawable.placeholder).circleCrop().into(binding.profileImage);
-            }*/
+                Glide.with(this).load(R.drawable.launcher1).circleCrop().into(binding.profileImage);
+            }
+
         } catch (Exception e) {
             Log.e(TAG, "Error: " + e);
         }
@@ -61,6 +89,31 @@ public class ProfileActivity extends AppCompatActivity {
     private void goEditProfileActivity() {
         Intent editIntent = new Intent(ProfileActivity.this, EditProfileActivity.class);
         startActivity(editIntent);
+    }
+
+    private void queryReviews() {
+        ParseQuery<ParseReview> query = ParseQuery.getQuery(ParseReview.class);
+        query.include(ParseReview.AUTHOR_KEY);
+        query.include(ParseReview.RESTAURANT_KEY);
+        query.whereEqualTo(ParseReview.AUTHOR_KEY, user);
+        query.setLimit(10);
+        query.addDescendingOrder(ParseReview.CREATED_AT_KEY);
+        query.findInBackground(new FindCallback<ParseReview>() {
+            @Override
+            public void done(List<ParseReview> reviewList, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting reviews");
+                    return;
+                }
+
+                for (ParseReview review : reviewList) {
+                    Log.i(TAG, "Post: " + review.getText() + ", username: " + review.getAuthor().getUsername());
+                }
+                allReviews.addAll(reviewList);
+                reviewAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 
 }
