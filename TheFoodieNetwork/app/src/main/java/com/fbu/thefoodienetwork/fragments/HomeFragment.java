0b901229531cada75e1e5014f4b70f4412c3cd71
Thu Vo <votranminhthu00@gmail.com>
2020-08-10 11:22:09 -0400

    empty message

--- TheFoodieNetwork/app/src/main/java/com/fbu/thefoodienetwork/fragments/HomeFragment.java
+++ TheFoodieNetwork/app/src/main/java/com/fbu/thefoodienetwork/fragments/HomeFragment.java
@@ -192,6 +192,11 @@ public class HomeFragment extends Fragment {
                     return;
                 }
 
+                if (reviewList.isEmpty()) {
+                    binding.emptyHomeView.setVisibility(View.VISIBLE);
+                    return;
+                }
+
                 for (ParseReview review : reviewList) {
 
                     review.getHeartRelation();
